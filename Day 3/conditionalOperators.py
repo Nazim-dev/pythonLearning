a999a4e62b8e39c940b50b820b5fcccc4dd2faa9
Nazim Phillips <sensephillips@gmail.com>
2022-09-07 08:28:33 -0400

    Conditionals and even/odd number checker

---
+++ Day 3/conditionalOperators.py
@@ -0,0 +1,8 @@
+print("Welcom to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the roller coaster!")
+else:
+    print("You can't ride the roller coaster!")
+
