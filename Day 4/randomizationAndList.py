1464ce269d312cc9e5969b0588a700500f1787c5
Nazim Phillips <sensephillips@gmail.com>
2022-09-08 08:29:56 -0400

    Randomization, list and head or tails

---
+++ Day 4/randomizationAndList.py
@@ -0,0 +1,14 @@
+# import random
+
+# random_integer = random.randint(1,10)
+
+# print(random_integer)
+
+# random_float = random.random()
+
+# print(random_float)
+
+
+windward_islands = ["Trinidad", "Grenada", "Saint Vincent", "Barbados", "Saint Lucia", "Dominica", "Martinique"]
+
+print(windward_islands)
