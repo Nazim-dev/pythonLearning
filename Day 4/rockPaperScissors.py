9160f33ab4992f2409122c607e3826b81e24ceea
Nazim Phillips <sensephillips@gmail.com>
2022-09-08 11:51:53 -0400

    update rock paper scissors code

--- Day 4/rockPaperScissors.py
+++ Day 4/rockPaperScissors.py
@@ -1,5 +1,4 @@
 #rock paper scissors game
-
 import random
 
 rock = '''
@@ -30,55 +29,23 @@ scissors = '''
 '''
 
 print("Welcom to Rock, Paper, Scissors!")
-
 playerChoice = int(input("Choose 0 for Rock, 1 for Paper and 2 for Scissors "))
-
+game_images = [rock, paper, scissors]
 compChoice = random.randint(0, 2)
 
-if playerChoice == 0 and compChoice == 0:
-    print(rock)
-    print("Computer chose: ")
-    print(rock)
+print("you chose: ")
+print(game_images[playerChoice])
+
+print("Computer chose: ")
+print(game_images[compChoice])
+
+if playerChoice == compChoice:
     print("Tie")
-elif playerChoice == 0 and compChoice == 1:
-    print(rock)
-    print("Computer chose: ")
-    print(paper)
-    print("You lose")
 elif playerChoice == 0 and compChoice == 2:
-    print(rock)
-    print("Computer chose: ")
-    print(scissors)
     print("You Win")
-
-elif playerChoice == 1 and compChoice == 1:
-    print(paper)
-    print("Computer chose: ")
-    print(paper)
-    print("Tie")
-elif playerChoice == 1 and compChoice == 2:
-    print(paper)
-    print("Computer chose: ")
-    print(scissors)
+elif playerChoice > compChoice:
+    print("You Win")
+elif playerChoice < compChoice:
     print("You lose")
-elif playerChoice == 1 and compChoice == 0:
-    print(paper)
-    print("Computer chose: ")
-    print(rock)
-    print("You Win")
-
-elif playerChoice == 2 and compChoice == 2:
-    print(scissors)
-    print("Computer chose: ")
-    print(scissors)
-    print("Tie")
 elif playerChoice == 2 and compChoice == 0:
-    print(scissors)
-    print("Computer chose: ")
-    print(rock)
     print("You lose")
-elif playerChoice == 2 and compChoice == 1:
-    print(scissors)
-    print("Computer chose: ")
-    print(paper)
-    print("You Win")
