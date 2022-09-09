703af1cf89bf1df523901e5ee7ea590140938e3f
Nazim Phillips <sensephillips@gmail.com>
2022-09-08 21:05:40 -0400

    High score

---
+++ Day 5/high_score.py
@@ -0,0 +1,14 @@
+#Calculate the highest score from a list
+
+student_scores = input("Input a list of student scores \n").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+highest = 0
+
+for score in student_scores:
+    if score > highest:
+        highest = score
+
+print(f"The highest score in the class is: {highest}")
