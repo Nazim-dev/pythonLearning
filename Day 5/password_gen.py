0da6011fe0c6ae51cf2cdd62786888eec28213a5
Nazim Phillips <sensephillips@gmail.com>
2022-09-09 08:48:57 -0400

    Updated password generator

--- Day 5/password_gen.py
+++ Day 5/password_gen.py
@@ -6,9 +6,18 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters= int(input("How many letters would you like in your password?\n"))
+if nr_letters < 1:
+    print("Invalid input \n")
+
 nr_symbols = int(input(f"How many symbols would you like?\n"))
+if nr_symbols < 1:
+    print("Invalid input \n")
+
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+if nr_numbers < 1:
+    print("Invalid input \n")
+
 
 password = []
 
