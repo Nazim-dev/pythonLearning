6feef59c6011979e5136c1eaf9b55cdb9a0701c3
Nazim Phillips <sensephillips@gmail.com>
2022-09-10 14:52:13 -0400

    functions

---
+++ Day 6/functions.py
@@ -0,0 +1,29 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+
+#final code for last challenge
+
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+         
+# while not at_goal():
+#     if front_is_clear() and right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     elif not wall_on_right():
+#         turn_right()
+#         move()
+#     elif wall_in_front():
+#         turn_left()
+#     else:
+#         turn_left()
