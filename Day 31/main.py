7573ba66ae6de5b0fb2736c2a0a4e2ca923fc443
Nazim Phillips <sensephillips@gmail.com>
2022-10-08 08:09:16 -0400

    Finished flash card app but switch my code with tutorial code

--- Day 31/main.py
+++ Day 31/main.py
@@ -1,55 +1,50 @@
 from cProfile import label
+from cgitb import text
 from random import randint
+from textwrap import fill
 from tkinter import *
 import pandas
-from setuptools import Command
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 LABEL_COLOR = "#FFFFFF"
 CARD_COLOR = "#91C2AF"
 FRENCH_COLOR = "#000000"
-english = None
-french = None
-card_words = None
+card = {}
+card_words = {}
+
+try:
+    data = pandas.read_csv("Day 31/data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("Day 31/data/french_words.csv")
+    card_words = data.to_dict(orient="records")
+else:
+    card_words = data.to_dict(orient="records")
+
+
+#Get card
+def get_word():
+    global card, switch_timer
+    window.after_cancel(switch_timer)
+    card = random.choice(card_words)
+    canvas.itemconfig(language, text="French", fill="black")
+    canvas.itemconfig(display_word, text=card["French"], fill="black")
+    canvas.itemconfig(canvas_image, image=card_front)
+    switch_timer = window.after(3000, func=switch_card)
+
 
 #Switch to english
 def switch_card():
+    canvas.itemconfig(language, text="English", fill="white")
+    canvas.itemconfig(display_word, text=card["English"], fill="white")
     canvas.itemconfig(canvas_image, image=card_back)
-    language.config(text="English", fg=LABEL_COLOR, bg=CARD_COLOR)
-    word.config(text=english, fg=LABEL_COLOR, bg=CARD_COLOR)
 
-#Delete word
-def delete_word():
-
-    for i in list(card_words["French"]):
-            if i == french:
-                del card_words["French"][i]
-
-    for i in list(card_words["English"]):
-        if i == english:
-            del card_words["English"][i]
-
-    cards_df = pandas.DataFrame(card_words)
-    cards_df.to_csv("Day 31/data/words_to_learn.csv")
-
-#Get csv
-def get_word():
-    global english
-    global french
-
-#use list instead of dictionary
-    with open("Day 31/data/french_words.csv") as file:
-        data = pandas.read_csv(file)
-        card_words = data.to_dict()
-        select = randint(0,101)
-        french = card_words["French"][select]
-        english = card_words["English"][select]
-
-        canvas.itemconfig(canvas_image, image=card_front)
-        language.config(text="French", bg=LABEL_COLOR, fg=FRENCH_COLOR)
-        word.config(text=french, bg=LABEL_COLOR, fg=FRENCH_COLOR)
-
-        window.after(3000, switch_card)
+#Delete card from list
+def delete_card():
+    card_words.remove(card) 
+    data = pandas.DataFrame(card_words)
+    data.to_csv("Day 31/data/words_to_learn.csv", index=False)
+    get_word()
 
 
 
@@ -58,29 +53,28 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=100, pady=50, bg=BACKGROUND_COLOR)
 
+switch_timer = window.after(3000, func=switch_card)
+
 #images
 incorrect = PhotoImage(file="Day 31/images/wrong.png")
 correct = PhotoImage(file="Day 31/images/right.png")
 card_back = PhotoImage(file="Day 31/images/card_back.png")
 card_front = PhotoImage(file="Day 31/images/card_front.png")
 
-#canvases
+#canvas
 canvas = Canvas(width=830, height=650, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas_image = canvas.create_image(420, 260, image=card_front)
+language = canvas.create_text(400, 150, text="", font=("Ariel", 30, "italic"))
+display_word = canvas.create_text(400, 270, text="", font=("Ariel", 50, "bold"))
 canvas.pack()
-
-#Labels
-language = Label(text="French", bg=LABEL_COLOR, font=("Ariel", 30, "italic"))
-language.place(x=325, y=75)
-
-word = Label(text="Ready?", bg=LABEL_COLOR, font=("Ariel", 50, "bold"))
-word.place(x=265, y=200)
 
 #buttons
 correct_button = Button(image=correct, borderwidth=0, bg=BACKGROUND_COLOR, command=get_word)
 correct_button.place(x=550, y=550,)
 
-incorrect_button = Button(image=incorrect, borderwidth=0, bg=BACKGROUND_COLOR, command=get_word)
+incorrect_button = Button(image=incorrect, borderwidth=0, bg=BACKGROUND_COLOR, command=delete_card)
 incorrect_button.place(x=150, y=550,)
+
+get_word()
 
 window.mainloop()
